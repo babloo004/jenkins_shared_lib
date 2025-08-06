b83fc248b579e68839f80f257cbd662e28d38616
Avinash <129788500+babloo004@users.noreply.github.com>
2025-08-06 23:08:25 +0530

    Create Person.groovy

---
+++ src/Person.groovy
@@ -0,0 +1,38 @@
+class Person{
+
+  //attributes
+  String fname;
+  String lname;
+  int age;
+  float weight;
+  float height;
+  def s;
+
+  //constructor
+
+  //default
+  Person(def s){
+    this.s = s;
+  }
+
+  //coustm
+  Person(String fname,String lname,int age,float weight,float height,def s){
+    this.fname=fname;
+    this.lname=lname;
+    this.age=age;
+    this.weight=weight;
+    this.height=height;
+    this.s=s;
+  }
+
+  //methods
+
+  //details
+  void details(){
+    s.echo "First Name : ${this.fname}";
+    s.echo "Last Name : ${this.lname}";
+    s.echo "Age : ${this.age}";
+    s.echo "Weight : ${this.weight}";
+    s.echo "Height : ${this.height}";
+  }
+}
